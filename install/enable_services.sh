5978c67020e873b946f924509323165962a96097
FernandoGaribay <garibaycejaf@gmail.com>
2024-07-17 15:19:05 -0600

    + Script Enable services

---
+++ install/enable_services.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo systemctl enable NetworkManager
+sudo systemctl enable pipewire
+sudo systemctl enable pipewire-pulse
+sudo systemctl enable wireplumber
+sudo systemctl enable sddm
