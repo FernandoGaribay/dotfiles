d0e0f61ccbe165e7493a25b266ef9d5d403b330a
FernandoGaribay <garibaycejaf@gmail.com>
2024-07-19 16:13:01 -0600

    + remove old config files from .config

--- install/symbolic_links.sh
+++ install/symbolic_links.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # qtile
+rm $HOME/.config/qtile -rf
 ln -s $HOME/Documents/dotfiles/.config/qtile $HOME/.config/qtile
 
 # fastfetch
+rm $HOME/.config/fastfetch -rf
 ln -s $HOME/Documents/dotfiles/.config/fastfetch $HOME/.config/fastfetch
 
 # picom fork ftlabs
+rm $HOME/.config/picom -rf
 ln -s $HOME/Documents/dotfiles/.config/picom $HOME/.config/picom
 
 # ranger
+rm $HOME/.config/ranger -rf
 ln -s $HOME/Documents/dotfiles/.config/ranger $HOME/.config/ranger
 
 # rofi
+rm $HOME/.config/rofi -rf
 ln -s $HOME/Documents/dotfiles/.config/rofi $HOME/.config/rofi
 
 # .zshrc
+rm $HOME/.zshrc
 ln -s $HOME/Documents/dotfiles/.config/.zshrc $HOME/.zshrc 
