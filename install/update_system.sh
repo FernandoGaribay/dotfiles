0bd3786e829a349a68393ff975c7770cb684d085
FernandoGaribay <garibaycejaf@gmail.com>
2024-07-19 16:08:16 -0600

    + .zshrc

--- install/update_system.sh
+++ install/update_system.sh
@@ -5,7 +5,7 @@ sudo pacman -Syu --noconfirm
 
 orphans=$(pacman -Qdtq)
 if [ -n "$orphans" ]; then
-  sudp pacman -Rns $orphans
+  sudo pacman -Rns $orphans
 else
   echo -e "\nNo orphan packages\n"
 fi
