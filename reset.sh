acededcfd6ec899645b7fdda4304d7aba19dba9e
FernandoGaribay <garibaycejaf@gmail.com>
2024-08-04 21:03:33 -0600

    + Script reset

---
+++ reset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Lista de paquetes a no borrar (fundamentales)
+essential_packages=(
+    'base'
+    'base-devel'
+    'linux'
+    'linux-firmware'
+    'grub'
+    'networkmanager'
+    'sudo'
+    'git'
+    'fastfetch'
+    'vim'
+    'cuda'
+    'lib32-nvidia-utils'
+    'lib32-opencl-nvidia'
+    'libvdpau'
+    'libxnvctrl'
+    'nvidia'
+    'nvidia-settings'
+    'nvidia-utils'
+    'opencl-nvidia'
+)
+
+# Marcar todos los paquetes instalados como dependencias
+sudo pacman -D --asdeps $(pacman -Qqe)
+
+# Marcar la lista de paquetes esenciales como explícitos
+sudo pacman -D --asexplicit "${essential_packages[@]}"
+
+# Borrar todos los paquetes que se hayan quedado como dependencias
+sudo pacman -Rsn $(pacman -Qttdq)
+
+# Limpiar la base de datos de paquetes
+sudo pacman -Scc --noconfirm
+
+# Preguntar al usuario si desea reiniciar el sistema
+read -p "¿Desea reiniciar el sistema ahora? (s/n): " answer
+case $answer in
+    [Ss]* ) sudo reboot;;
+    [Nn]* ) echo "El sistema no se reiniciará."; exit;;
+    * ) echo "Respuesta no válida. El sistema no se reiniciará."; exit;;
+esac
