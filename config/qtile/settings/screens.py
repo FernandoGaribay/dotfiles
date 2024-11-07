80892a5c5a332dc3c73106895d03c96ee8fd64bc
FernandoGaribay <garibaycejaf@gmail.com>
2024-11-07 09:01:36 -0600

    + Update Qtile Config

--- config/qtile/settings/screens.py
+++ config/qtile/settings/screens.py
@@ -1,4 +1,4 @@
-from libqtile import bar, layout
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Screen, Match
 from libqtile.lazy import lazy
 
@@ -6,146 +6,97 @@ from settings.variables import *
 from settings.layouts import *
 from settings.widgets import *
 
-decor_left = {
-    "decorations": [
-        PowerLineDecoration(
-            path="arrow_left"
-            # path="rounded_left"
-            # path="forward_slash"
-            # path="back_slash"
-        )
-    ],
-}
-
-decor_right = {
-    "decorations": [
-        PowerLineDecoration(
-            path="arrow_right"
-            # path="rounded_right"
-            # path="forward_slash"
-            # path="back_slash"
-        )
-    ],
-}
-
-powerline = {
-    "decorations": [
-        PowerLineDecoration()
-    ]
-}
+powerline = {"decorations": [PowerLineDecoration()]}
 
 screens = [
     Screen(
         top=bar.Bar(
             [
-                # Imagen Alacritty -----------------
-                Spacer(5, "#1793D1"), # #1793D1
-                widget.Image(
-                    **decor_left,
-                    filename="~/.config/qtile/archlinux.png",
-                    margin=5,
-                    background="#1793D1",
-                    mouse_callbacks = {
-                        'Button1': lazy.spawn("kitty"),
-                    }
-                ),
-
-                # Groups ---------------------------
-
+                Spacer(5, "#1793D1"),
+                ArchLogo(),
                 Spacer(5, Color2),
-
-                widget.GroupBox(
-                    **decor_left,
-                    font = "JetBrainsMono Nerd Font",
-                    fontsize=10,
-                    active=Foreground,
-                    inactive=Foreground,
-                    background=Color2,
-                    this_current_screen_border=darken_color(Color2, 0.5),
-                    other_current_screen_border=darken_color(Color2, 0.5), 
-                    other_screen_border=lighten_color(Color2, 0.25),
-                    this_screen_border=lighten_color(Color2, 0.25),
-                    borderwidth=5,
-                    highlight_method='block',
-                    disable_drag=True,
-                    center_aligned=True,
-                ),
-                Spacer(10, colorBarra), 
-
-                # Window name ----------------------
+                GroupBox(),
+                Spacer(10, colorBarra),
                 widget.Prompt(),
-                widget.WindowName(
-                    max_chars = 40
-                ),
-                widget.Spacer(),
-                widget.Spacer(
+                widget.WindowName(max_chars=40),
+                widget.Spacer(**decor_right, length=30),
+                CurrentLayout(),
+                get_battery_widget(),
+                get_active_connection_widget(),
+                widget.Net(
                     **decor_right,
-                    length=30
-                ),
-
-                # Temperaturas ---------------------
-                widget.CPU(
-                    **decor_right,
-                    fmt='  <b>{}</b>',
-                    background=Color3,
-                    fontshadow=colorNegro,
-                ),
-
-                widget.ThermalSensor(
-                    **decor_right,
-                    tag_sensor='Tctl',
-                    format=' <b>{temp:.0f}{unit}</b>',
+                    format='󰓅  <b>{down:.0f}{down_suffix} ↓↑ {up:.0f}{up_suffix}</b>',
+                    padding=10,
                     background=Color4,
                     fontshadow=colorNegro,
                 ),
-
-                widget.CurrentLayout(
-                    **decor_right,
-                    fmt='  <b>{}</b>',
-                    background=Color5,
-                    fontshadow=colorNegro,
-                ),
-                
-                widget.Clock(
-                    **decor_right,
-                    format="%Y-%m-%d %a %I:%M %p",
-                    fmt='󰃰  <b>{}</b>',
-                    background=Color6,
-                    fontshadow=colorNegro,
-                ),
-
-                widget.Systray(
-                    background=Color7
-                ),
-                
+                Clock(),
+                widget.Systray(background=Color7),
                 Spacer(10, Color7),
-
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
             ],
             tamanoBarra,
             background=colorBarra,
-            margin=[5,10,5,10],
+            margin=[5, 10, 5, 10],
             opacity=1,
             border_width=[0, 0, 0, 0],
-            border_color=["ffffff", "ffffff", "ffffff", "ffffff"]
+            border_color=["ffffff", "ffffff", "ffffff", "ffffff"],
         ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
         # x11_drag_polling_rate = 60,
     ),
-
     Screen(
         top=bar.Bar(
             [
-                widget.WindowName(),
+                Spacer(5, "#1793D1"),
+                ArchLogo(),
+                Spacer(5, Color2),
+                GroupBox(),
+                Spacer(10, colorBarra),
+                widget.WindowName(max_chars=40),
+                widget.Spacer(**decor_right, length=30),
+
+                CurrentLayout(),
+                widget.CPU(
+                    fmt='  <b>{}</b>',
+                    background=Color3,
+                    fontshadow=colorNegro,
+                ),
+                widget.ThermalSensor(
+                    **decor_right,
+                    tag_sensor='Tctl',
+                    format=' <b>{temp:.0f}{unit}</b>',
+                    background=Color3,
+                    padding=10,
+                    fontshadow=colorNegro,
+                ),
+                widget.NvidiaSensors(
+                    **decor_right,
+                    threshold=75, foreground_alert='ff6000',
+                    fmt="󰺵  <b>{}</b>",
+                    padding=10,
+                    background=Color5,
+                    fontshadow=colorNegro,
+                ),
+                widget.Memory(
+                    **decor_right,
+                    fmt=" <b>{}</b>",
+                    padding=10,
+                    background=Color4,
+                    fontshadow=colorNegro,
+                ),
+                Spacer(10, Color7),
             ],
             tamanoBarra,
             background=colorBarra,
+            margin=[5, 10, 5, 10],
+            opacity=1,
+            border_width=[0, 0, 0, 0],
+            border_color=["ffffff", "ffffff", "ffffff", "ffffff"],
         ),
-        #layouts=[
-        #    layout.Max(),
-        #],
     ),
 ]
+
--- config/qtile/settings/widgets.py
+++ config/qtile/settings/widgets.py
@@ -6,13 +6,95 @@ from qtile_extras import widget
 from qtile_extras.widget.decorations import PowerLineDecoration
 
 from settings.variables import *
+import os
+import subprocess
+
+decor_left = {
+    "decorations": [
+        PowerLineDecoration(
+            path="arrow_left"
+            # path="rounded_left"
+            # path="forward_slash"
+            # path="back_slash"
+        )
+    ],
+}
+
+decor_right = {
+    "decorations": [
+        PowerLineDecoration(
+            path="arrow_right"
+            # path="rounded_right"
+            # path="forward_slash"
+            # path="back_slash"
+        )
+    ],
+}
+
+def get_battery_widget():
+    battery_path = "/sys/class/power_supply/BAT0/"
+    if os.path.exists(battery_path):
+        return widget.Battery()
+    else:
+        return widget.TextBox(
+            **decor_right,
+            padding=10,
+            background=Color2,
+            fontshadow=colorNegro,
+            fmt='<b>󰂄 Connected</b>'
+        )
+
+def get_active_connection_widget():
+    return widget.TextBox(
+        fmt=f"󱚽  <b>{_get_active_connection()}</b>",
+        padding=5,
+        background=Color4,
+        fontshadow=colorNegro,
+    )
+
+def _get_active_connection():
+    output = subprocess.check_output("nmcli -t -f CONNECTION device", shell=True).decode('utf-8').strip()
+    connections = output.split('\n')
+    for conn in connections:
+        if conn and "Wired" in conn:
+            return conn
+    return "No Connection"
 
 def Spacer(length=10, background=colorBarra):
-    return  widget.Spacer(
+    return widget.Spacer(
         length=length,
         background=background,
     ) 
     
+def ArchLogo():
+    return widget.Image(
+        **decor_left,
+        filename="~/.config/qtile/archlinux.png",
+        margin=5,
+        background="#1793D1",
+        mouse_callbacks={
+            "Button1": lazy.spawn("kitty"),
+        },
+    )
+
+def GroupBox():
+    return widget.GroupBox(
+        **decor_left,
+        font="JetBrainsMono Nerd Font",
+        fontsize=10,
+        active=Foreground,
+        inactive=Foreground,
+        background=Color2,
+        this_current_screen_border=darken_color(Color2, 0.5),
+        other_current_screen_border=darken_color(Color2, 0.5),
+        other_screen_border=lighten_color(Color2, 0.25),
+        this_screen_border=lighten_color(Color2, 0.25),
+        borderwidth=5,
+        highlight_method="block",
+        disable_drag=True,
+        center_aligned=True,
+    )
+
 def TextIcon(text="", padding=0, foreground=colorIMG, background=colorBlanco, fontshadow=colorNegro, fontsize=36):
     return widget.TextBox(
         text=text,
@@ -21,4 +103,23 @@ def TextIcon(text="", padding=0, foreground=colorIMG, background=colorBlanco,
         background=background,
         fontshadow=fontshadow,
         fontsize=fontsize,
+    )
+
+def CurrentLayout():
+    return widget.CurrentLayout(
+        **decor_right,
+        fmt="  <b>{}</b>",
+        padding=10,
+        background=Color5,
+        fontshadow=colorNegro,
+    )
+
+def Clock():
+    return widget.Clock(
+        **decor_right,
+        format="%Y-%m-%d %a %I:%M %p",
+        fmt="󰃰  <b>{}</b>",
+        padding=10,
+        background=Color6,
+        fontshadow=colorNegro,
     )
