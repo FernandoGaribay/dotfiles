f06bca826b5e7301df1e43806fb9bbc8195ec980
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:46:54 -0600

    + install_fonts.sh updated

--- install/install_fonts.sh
+++ install/install_fonts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+source /home/$(whoami)/.dotfiles/install/colors.sh
 source /home/$(whoami)/.dotfiles/install/packages/fonts.sh
 
 for i in "${fonts[@]}"; do
   if pacman -Si "$i" &>/dev/null; then
+    echo -e "  > Installing ${DARK_GREEN}font ${DARK_BLUE}'$i'${NC}..."
     sudo pacman -S --noconfirm $i
   else
-    echo "$i NO existe en los repositorios"
+    echo -e "  > ${RED_ERROR}Error:${NC} The ${DARK_BLUE}font${NC} ${DARK_GREEN}'$i'${NC} is not available in the repositories.\n"
   fi
 done
+echo -e "  > All ${DARK_BLUE}fonts${NC} have been ${DARK_GREEN}installed${NC}."
