2eeda95f73495a866353570f0cf8c6dc2687b228
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:48:21 -0600

    + install_yay.sh updated

--- install/install_yay.sh
+++ install/install_yay.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+source /home/$(whoami)/.dotfiles/install/colors.sh
+
+if ! command -v yay &>/dev/null; then
+    echo -e "  > ${DARK_YELLOW}yay${NC} ${RED_ERROR}is not installed${NC}. Installing ${DARK_YELLOW}yay${NC}..."
+
+    echo -e "  > ${DARK_BLUE}Cloning${NC} ${DARK_YELLOW}yay${NC} repository from ${DARK_GREEN}AUR${NC}...\n"
+    git clone https://aur.archlinux.org/yay.git
+
+    echo -e "  > ${DARK_BLUE}Changing${NC} to the yay directory..."
+    cd /home/$(whoami)/yay
+
+    echo -e "  > ${DARK_BLUE}Building${NC} ${DARK_YELLOW}yay${NC} package with makepkg...\n"
+    makepkg -si --noconfirm
+
+    cd ..
+    rm -rf /home/$(whoami)/yay
+    echo -e "  > ${DARK_YELLOW}yay${NC} has been ${DARK_GREEN}installed${NC}."
+  
+else
+  echo -e "  > ${DARK_YELLOW}yay${NC} ${DARK_GREEN}is already installed${NC}."
+fi
