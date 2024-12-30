a322f7c3ba20a727cc310cb0d56130545c315ddd
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:48:35 -0600

    + orphans_packages.sh updated

--- install/orphans_packages.sh
+++ install/orphans_packages.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+source /home/$(whoami)/.dotfiles/install/colors.sh
+
 orphans=$(pacman -Qdtq)
 if [ -n "$orphans" ]; then
+  echo -e "  > ${DARK_BLUE}Unistalling${NC} the following ${DARK_GREEN}packages${NC}...\n\n$orphans"
+  sleep 1
   sudo pacman -Rns $orphans
 else
-  echo -e "\nNo orphan packages\n"
+  echo -e "\n  > No orphan ${DARK_GREEN}packages${NC}."
 fi
