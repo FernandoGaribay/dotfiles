bf249a83f4d8ca03f3479082447c424afec6d8ba
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:47:12 -0600

    + install_sddm_themes.sh updated

--- install/install_sddm_themes.sh
+++ install/install_sddm_themes.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo cp $HOME/.dotfiles/themes/sddm-themes/Sonomatic /usr/share/sddm/themes -r
+source /home/$(whoami)/.dotfiles/install/colors.sh
+
+echo -e "  > Installing ${DARK_GREEN}theme ${DARK_BLUE}'Sonomatic'${NC} on ${DARK_YELLOW}/usr/share/sddm/themes${NC}..."
+
+sudo cp -r $HOME/.dotfiles/themes/sddm-themes/Sonomatic /usr/share/sddm/themes
+echo "xrandr --output DP-1 --off" | sudo tee -a /usr/share/sddm/scripts/Xsetup >/dev/null
