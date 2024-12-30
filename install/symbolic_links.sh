dfd67432c1de5a4f07cdd30a7c15355c033365a8
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:48:50 -0600

    + symbolic_links.sh updated

--- install/symbolic_links.sh
+++ install/symbolic_links.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
+source /home/$(whoami)/.dotfiles/install/colors.sh
+
 # qtile
 ln -sf $HOME/.dotfiles/config/qtile $HOME/.config/qtile
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/qtile${NC} -> ${DARK_YELLOW}$HOME/.config/qtile${NC}"
+
 ln -sf $HOME/.dotfiles/config/rofi $HOME/.config/qtile/rofi-menus/rofi
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/rofi${NC} -> ${DARK_YELLOW}$HOME/.config/qtile/rofi-menus/rofi${NC}"
 
 # fastfetch
 ln -sf $HOME/.dotfiles/config/fastfetch $HOME/.config/fastfetch
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/fastfetch${NC} -> ${DARK_YELLOW}$HOME/.config/fastfetch${NC}"
 
 # picom fork ftlabs
 ln -sf $HOME/.dotfiles/config/picom $HOME/.config/picom
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/picom${NC} -> ${DARK_YELLOW}$HOME/.config/picom${NC}"
 
 # ranger
 ln -sf $HOME/.dotfiles/config/ranger $HOME/.config/ranger
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/ranger${NC} -> ${DARK_YELLOW}$HOME/.config/ranger${NC}"
 
 # rofi
 ln -sf $HOME/.dotfiles/config/rofi $HOME/.config/rofi
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/rofi${NC} -> ${DARK_YELLOW}$HOME/.config/rofi${NC}"
 
 # .zshrc
 ln -sf $HOME/.dotfiles/config/.zshrc $HOME/.zshrc
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/.zshrc${NC} -> ${DARK_YELLOW}$HOME/.zshrc${NC}"
 
 # SDDM config file
 sudo ln -sf $HOME/.dotfiles/config/sddm.conf /etc/sddm.conf
-
-# GTK Themes
-if [ ! -d "$HOME/.themes" ]; then
-  mkdir $HOME/.themes
-fi
-ln -sf $HOME/.dotfiles/themes/gtk-themes/DraculaTheme $HOME/.themes/DraculaTheme
-
-# GTK Icons and cursors
-if [ ! -d "$HOME/.icons" ]; then
-  mkdir $HOME/.icons
-fi
-# icons
-ln -sf $HOME/.dotfiles/themes/icons/Wings-Light-Icons $HOME/.icons/Wings-Light-Icons
-#cursors
-ln -sf $HOME/.dotfiles/themes/cursors/Bibata-Modern-Ice $HOME/.icons/Bibata-Modern-Ice
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/sddm.conf${NC} -> ${DARK_YELLOW}/etc/sddm.conf${NC}"
