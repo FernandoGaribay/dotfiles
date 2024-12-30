6b5d22170809604a171f21ea8e2c5b5d4d12b50b
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-29 22:48:11 -0600

    + install_yay_packages.sh updated

--- install/install_yay_packages.sh
+++ install/install_yay_packages.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+source /home/$(whoami)/.dotfiles/install/colors.sh
 source /home/$(whoami)/.dotfiles/install/packages/yay_packages.sh
 
 for i in "${packagesYay[@]}"; do
   if yay -Si "$i" &>/dev/null; then
+    echo -e "  > Installing ${DARK_GREEN}package ${DARK_BLUE}'$i'${NC}..."
     yay -S --noconfirm $i
   else
-    echo "$i NO existe en los repositorios."
+    echo -e "  > ${RED_ERROR}Error:${NC} The ${DARK_BLUE}package${NC} ${DARK_GREEN}'$i'${NC} is not available in the repositories.\n"
   fi
 done
+echo -e "  > All ${DARK_BLUE}packages${NC} have been ${DARK_GREEN}installed${NC}."
