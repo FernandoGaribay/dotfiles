8158124a504aae94cb1993588eca49681b91a8f6
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-30 20:05:58 -0600

    + Show list of orphaned packages

--- install/orphans_packages.sh
+++ install/orphans_packages.sh
@@ -4,8 +4,14 @@ source /home/$(whoami)/.dotfiles/install/colors.sh
 
 orphans=$(pacman -Qdtq)
 if [ -n "$orphans" ]; then
-  echo -e "  > ${DARK_BLUE}Unistalling${NC} the following ${DARK_GREEN}packages${NC}...\n\n$orphans"
+  echo -e "  > ${DARK_BLUE}Unistalling${NC} the following ${DARK_GREEN}orphans packages${NC}...\n"
   sleep 1
+
+  IFS=$'\n' read -rd '' -a orphans_array <<< "$orphans"
+  for i in "${orphans_array[@]}"; do
+    echo "  * ${DARK_YELLOW}${i}${NC}"
+  done
+  echo -e "\n"
   sudo pacman -Rns $orphans
 else
   echo -e "\n  > No orphan ${DARK_GREEN}packages${NC}."
