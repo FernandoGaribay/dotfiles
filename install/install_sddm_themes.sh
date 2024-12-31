65889aaadcc2f9aa160df0de22c7138a2edaf92d
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-30 19:42:45 -0600

    + Installation completion message

--- install/install_sddm_themes.sh
+++ install/install_sddm_themes.sh
@@ -6,3 +6,5 @@ echo -e "  > Installing ${DARK_GREEN}theme ${DARK_BLUE}'Sonomatic'${NC} on ${DAR
 
 sudo cp -r $HOME/.dotfiles/themes/sddm-themes/Sonomatic /usr/share/sddm/themes
 echo "xrandr --output DP-1 --off" | sudo tee -a /usr/share/sddm/scripts/Xsetup >/dev/null
+
+echo -e "  > Theme ${DARK_BLUE}'Sonomatic'${NC} ${DARK_GREEN}installed${NC}..."
