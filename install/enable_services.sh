16d02a216d1ff241a61f7acf7e51c74ad46b7d74
FernandoGaribay <garibaycejaf@gmail.com>
2024-12-30 19:39:18 -0600

    + enable_services.sh updated

--- install/enable_services.sh
+++ install/enable_services.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+source /home/$(whoami)/.dotfiles/install/colors.sh
+
 sudo systemctl enable NetworkManager
+echo -e "\n  > ${DARK_GREEN}NetworkManager ${DARK_BLUE}service${NC} enabled.\n"
+
 sudo systemctl enable sddm
+echo -e "\n  > ${DARK_GREEN}SDDM ${DARK_BLUE}service${NC} enabled.\n"
+
 sudo systemctl start libvirtd
+echo -e "\n  > ${DARK_GREEN}libvirtd ${DARK_BLUE}service${NC} enabled.\n"
+
 
 sudo usermod -aG libvirt $(whoami)
+echo -e "\n  > User: ${DARK_GREEN}$USER${NC} added to group ${DARK_BLUE}'libvirt'${NC}."
