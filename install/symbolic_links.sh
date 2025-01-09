0d6ef1e16e637e9c5795334e6839ed0684777ff2
FernandoGaribay <garibaycejaf@gmail.com>
2025-01-09 09:13:36 -0600

    + Dunst config

--- install/symbolic_links.sh
+++ install/symbolic_links.sh
@@ -22,6 +22,10 @@ echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/c
 ln -sf $HOME/.dotfiles/config/rofi $HOME/.config/rofi
 echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/rofi${NC} -> ${DARK_YELLOW}$HOME/.config/rofi${NC}"
 
+# dunst
+ln -sf $HOME/.dotfiles/config/dunst $HOME/.config/dunst
+echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/dunst${NC} -> ${DARK_YELLOW}$HOME/.config/dunst${NC}"
+
 # .zshrc
 ln -sf $HOME/.dotfiles/config/.zshrc $HOME/.zshrc
 echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$HOME/.dotfiles/config/.zshrc${NC} -> ${DARK_YELLOW}$HOME/.zshrc${NC}"
