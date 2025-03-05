0997bbb2b330e9f3951fd50f50091c167a8e1e61
FernandoGaribay <garibaycejaf@gmail.com>
2025-03-05 12:58:45 -0600

    removed: libvirtd service deleted

--- install/enable_services.sh
+++ install/enable_services.sh
@@ -7,10 +7,3 @@ echo -e "\n  > ${DARK_GREEN}NetworkManager ${DARK_BLUE}service${NC} enabled.\n"
 
 sudo systemctl enable sddm
 echo -e "\n  > ${DARK_GREEN}SDDM ${DARK_BLUE}service${NC} enabled.\n"
-
-sudo systemctl start libvirtd
-echo -e "\n  > ${DARK_GREEN}libvirtd ${DARK_BLUE}service${NC} enabled.\n"
-
-
-sudo usermod -aG libvirt $(whoami)
-echo -e "\n  > User: ${DARK_GREEN}$USER${NC} added to group ${DARK_BLUE}'libvirt'${NC}."
