5ab1ea85874eb9d4350a6668c37c550077a1f602
Fernando <garibaycejaf@gmail.com>
2025-05-27 00:38:37 -0600

    fix: temp directory was not being created

--- install/gkt_theme.sh
+++ install/gkt_theme.sh
@@ -13,10 +13,20 @@ if ! [ -d $THEMES_DIR ]; then
     mkdir $THEMES_DIR
 fi
 
+if ! [ -d $TEMP_DIR ]; then
+    echo -e "\n  > Temp Directory: $TEMP_DIR created."
+    sudo mkdir -p $TEMP_DIR
+    sudo chown -R $(whoami):$(whoami) "$TEMP_DIR"
+fi
+
+sudo pacman -S jq --noconfirm
+
 echo -e "\n  > Downloading GTK 3 $THEME_NAME..."
 curl -s https://api.github.com/repos/$THEME_REPOSITORY/releases/latest \
 | jq -r ".assets[] | select(.name == \"$THEME_NAME\") | .browser_download_url" \
 | xargs curl -L -o "$TEMP_DIR/$THEME_NAME"
+
+sudo pacman -Rsn jq --noconfirm
 
 echo -e "\n  > Installing theme $THEME_NAME...\n"
 tar -xJf $TEMP_DIR/$THEME_NAME -C $THEMES_DIR
--- install/install_sddm_themes.sh
+++ install/install_sddm_themes.sh
@@ -13,10 +13,20 @@ if ! [ -d $THEMES_DIR ]; then
     sudo mkdir -p $THEMES_DIR
 fi
 
+if ! [ -d $TEMP_DIR ]; then
+    echo -e "\n  > Temp Directory: $TEMP_DIR created."
+    sudo mkdir -p $TEMP_DIR
+    sudo chown -R $(whoami):$(whoami) "$TEMP_DIR"
+fi
+
+sudo pacman -S jq --noconfirm
+
 echo -e "\n  > Downloading SDDM Theme $THEME_NAME..."
 curl -s https://api.github.com/repos/$THEME_REPOSITORY/releases/latest \
 | jq -r ".assets[] | select(.name == \"$THEME_NAME\") | .browser_download_url" \
 | xargs curl -L -o "$TEMP_DIR/$THEME_NAME"
+
+sudo pacman -Rsn jq --noconfirm
 
 echo -e "\n  > Installing theme $THEME_NAME...\n"
 sudo unzip $TEMP_DIR/$THEME_NAME -d $THEMES_DIR
