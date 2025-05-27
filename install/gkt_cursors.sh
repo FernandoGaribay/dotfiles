a456b99bd4869800e57ed7b88dbe9c3ff4e20dac
FernandoGaribay <garibaycejaf@gmail.com>
2025-05-26 22:19:52 -0600

    refactor: gkt_cursors.sh script

--- install/gkt_cursors.sh
+++ install/gkt_cursors.sh
@@ -2,29 +2,24 @@
 
 source /home/$(whoami)/.dotfiles/install/colors.sh
 
-GTK3_FILE="$HOME/.config/gtk-3.0/settings.ini"
-GTK2_FILE="$HOME/.gtkrc-2.0"
-GTK_DEFAULT="/usr/share/icons/default/index.theme"
+THEMES_DIR="$HOME/.icons"
+TEMP_DIR="$HOME/.dotfiles/temp"
 
-SOURCE_DIR="$HOME/.dotfiles/themes/cursors"
-DEST_DIR="$HOME/.icons"
+THEME_REPOSITORY="ful1e5/Bibata_Cursor"
+THEME_NAME="Bibata-Modern-Ice.tar.xz"
 
-for cursor in "$SOURCE_DIR"/*; do
-  cursor_theme=$(basename "$cursor")
-  ln -sf $HOME/.dotfiles/themes/cursors/$cursor_theme $DEST_DIR
-  echo "  > ${DARK_BLUE}Symbolic link${NC} created: ${DARK_GREEN}$cursor${NC} -> ${DARK_YELLOW}$DEST_DIR/$cursor_theme${NC}"
-done
-
-if [ ! -f "$GTK3_FILE" ]; then
-    echo "${DARK_BLUE}  > File $GTK3_FILE does not exist.${NC} "
-else
-    sed -i '/gtk-cursor-theme-name=/c\gtk-cursor-theme-name=Bibata-Modern-Ice' "$GTK3_FILE"
+if ! [ -d $THEMES_DIR ]; then
+    echo -e "\n  > Directory: $THEMES_DIR created."
+    mkdir $THEMES_DIR
 fi
 
+echo -e "\n  > Downloading GTK 3 $THEME_NAME..."
+curl -s https://api.github.com/repos/$THEME_REPOSITORY/releases/latest \
+| jq -r ".assets[] | select(.name == \"$THEME_NAME\") | .browser_download_url" \
+| xargs curl -L -o "$TEMP_DIR/$THEME_NAME"
 
-if [ ! -f "$GTK2_FILE" ]; then
-    echo "${DARK_BLUE}  > File $GTK2_FILE does not exist.${NC} "
-else
-    sed -i '/gtk-cursor-theme-name=/c\gtk-cursor-theme-name="Bibata-Modern-Ice"' "$GTK2_FILE"
-fi
-sudo sed -i '/Inherits=/c\Inherits=Bibata-Modern-Ice' "$GTK_DEFAULT"
+echo -e "\n  > Installing theme $THEME_NAME...\n"
+tar -xJf $TEMP_DIR/$THEME_NAME -C $THEMES_DIR
+
+
+
